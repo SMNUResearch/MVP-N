ec7cb5ddbb530512d9c72edb44fe8a87f4148fa5
SMNUResearch <106851760+SMNUResearch@users.noreply.github.com>
2026-01-20 05:32:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pip3 install numpy==1.22.0 --user
 pip3 install Pillow==7.0.0 --user
 pip3 install matplotlib==3.5.2 --user
 pip3 install termcolor==1.1.0 --user
-pip3 install thop==0.0.31.post2005241907 --user
+pip3 install thop==0.1.1.post2209072238 --user
 pip3 install einops==0.6.1 --user
 pip3 install scikit-learn==1.2.2 --user
 pip3 install PyYAML==6.0 --user
